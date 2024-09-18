608de5d533420185ad9e841e78409bfd50c337fb
nkc20236039 <nkc23.kawai.yuto@gmail.com>
2024-09-18 13:54:47 +0900

    [更新] ワールド生成オブジェクトをプレハブ化

--- Assets/Scripts/World/Interface/IChunkInformation.cs
+++ Assets/Scripts/World/Interface/IChunkInformation.cs
@@ -16,4 +16,11 @@ public interface IChunkInformation
     /// <param name="position">ワールド座標</param>
     /// <returns>チャンクのタイルマップオブジェクト</returns>
     public Tilemap GetChunk(Vector2 position, Vector2Int chunkVector);
+
+    /// <summary>
+    /// 指定の座標の層番号を取得する
+    /// </summary>
+    /// <param name="position">ワールド座標</param>
+    /// <returns>層の番号(上が0)</returns>
+    public int GetLayer(Vector2 position);
 }
