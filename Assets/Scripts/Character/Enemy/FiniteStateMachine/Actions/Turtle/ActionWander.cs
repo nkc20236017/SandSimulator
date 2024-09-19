e49796c195b1a4bd9ffa98d2d27460206fa06e81
nkc20236031 <nkc23.watanabe.mio@gmail.com>
2024-09-19 23:51:57 +0900

    ［修正］カメの進行方向に１ブロック以上の壁があったら向きを反転する

--- Assets/Scripts/Character/Enemy/FiniteStateMachine/Actions/Turtle/ActionWander.cs
+++ Assets/Scripts/Character/Enemy/FiniteStateMachine/Actions/Turtle/ActionWander.cs
@@ -18,9 +18,6 @@ public class ActionWander : FsmAction
 	[SerializeField] private bool autoJump;
 	[ShowIf(nameof(autoJump))]
 	[SerializeField] private int autoJumpHeight;
-
-	[Header("Wall Config")]
-	[SerializeField] private int wallHeight;
 	
 	[Header("Fall Config")]
 	[SerializeField] private bool matchTheColliderDirectionFall;
@@ -63,13 +60,18 @@ public class ActionWander : FsmAction
 		if (_rigidbody2D.velocity.y is > 0.01f or < -0.01f) { return; }
 		if (_moveDirection.x == 0) { return; }
 		if (!IsGround()) { return; }
+		if (IsWall()) { return; }
+		if (IsHeavenly()) { return; }
 
 		var x = _moveDirection.x >= 0 ? _boxCollider2D.bounds.max.x + 0.25f : _boxCollider2D.bounds.min.x - 0.25f;
-		var position = new Vector2(x, _boxCollider2D.bounds.min.y + autoJumpHeight - 1);
-		var cellPosition = _tilemap.WorldToCell(position);
-		if (_tilemap.HasTile(cellPosition) && !_tilemap.HasTile(cellPosition + Vector3Int.up))
+		for (var y = 0; y < autoJumpHeight; y++)
 		{
-			transform.position += new Vector3(0.25f, autoJumpHeight + 0.25f, 0);
+			var position = new Vector2(x, _boxCollider2D.bounds.min.y + y);
+			var cellPosition = _tilemap.WorldToCell(position);
+			if (!_tilemap.HasTile(cellPosition) || _tilemap.HasTile(cellPosition + Vector3Int.up)) { continue; }
+
+			transform.position += new Vector3(0.25f, y + 1.25f, 0);
+			break;
 		}
 	}
 	
@@ -80,6 +82,15 @@ public class ActionWander : FsmAction
 		var position = new Vector2(x, y);
 		var hit = matchTheColliderDirectionGround ? Physics2D.CircleCast(position, _boxCollider2D.size.x / 2 - 0.1f, Vector2.down, 0.1f, groundLayerMask) : Physics2D.CircleCast(position, radius, Vector2.down, 0.1f, groundLayerMask);
 		return hit.collider != null;
+	}
+	
+	private bool IsHeavenly()
+	{
+		var x = _boxCollider2D.bounds.center.x;
+		var y = _boxCollider2D.bounds.max.y;
+		var position = new Vector2(x, y);
+		var hit = matchTheColliderDirectionFall ? Physics2D.CircleCast(position, fallDirection.x, Vector2.up, fallDirection.y, groundLayerMask) : Physics2D.CircleCast(position, radius, Vector2.up, 0.1f, groundLayerMask);
+		return hit.collider == null;
 	}
 	
 	private void Flip()
@@ -98,7 +109,7 @@ public class ActionWander : FsmAction
 		var x = _moveDirection.x >= 0 ? _boxCollider2D.bounds.max.x + _boxCollider2D.bounds.size.x / 2 + 0.25f : _boxCollider2D.bounds.min.x - _boxCollider2D.bounds.size.x / 2 - 0.25f;
 		var position = new Vector2(x, _boxCollider2D.bounds.min.y - _boxCollider2D.bounds.size.y / 2 - 0.1f);
 		var cellPosition = _tilemap.WorldToCell(position);
-		var bounds = new BoundsInt(cellPosition, Vector3Int.RoundToInt(_boxCollider2D.size));
+		var bounds = new BoundsInt(cellPosition, Vector3Int.CeilToInt(_boxCollider2D.size));
 		return _tilemap.GetTilesBlock(bounds) == null;
 	}
 
@@ -108,9 +119,17 @@ public class ActionWander : FsmAction
 		if (_moveDirection.x == 0) { return false; }
 		
 		var x = _moveDirection.x >= 0 ? _boxCollider2D.bounds.max.x + 0.25f : _boxCollider2D.bounds.min.x - 0.25f;
-		var position = new Vector2(x, _boxCollider2D.bounds.min.y + wallHeight - 1);
-		var cellPosition = _tilemap.WorldToCell(position);
-		return _tilemap.HasTile(cellPosition);
+		var wallHeight = _boxCollider2D.bounds.size.y - (autoJump ? autoJumpHeight : 0);
+		for (var y = 0; y < wallHeight; y++)
+		{
+			var position = new Vector2(x, _boxCollider2D.bounds.min.y + (autoJump ? autoJumpHeight : 0) + y);
+			var cellPosition = _tilemap.WorldToCell(position);
+			if (!_tilemap.HasTile(cellPosition)) { continue; }
+			
+			return true;
+		}
+		
+		return false;
 	}
 	
 	private void Movement()
@@ -134,26 +153,31 @@ public class ActionWander : FsmAction
 		x = _moveDirection.x >= 0 ? boxCollider2D.bounds.max.x + 0.25f : boxCollider2D.bounds.min.x - 0.25f;
 		
 		// ジャンプのギズモ表示
-		position = new Vector2(x, boxCollider2D.bounds.min.y + autoJumpHeight - 1);
-		var cellPosition = _tilemap.WorldToCell(position);
-		Gizmos.color = _tilemap.HasTile(cellPosition) && !_tilemap.HasTile(cellPosition + Vector3Int.up) ? Color.red : Color.green;
-		Gizmos.DrawWireCube(_tilemap.GetCellCenterWorld(cellPosition), Vector3.one);
+		for (y = 0; y < autoJumpHeight; y++)
+		{
+			position = new Vector2(x, boxCollider2D.bounds.min.y + y);
+			var cellPosition0 = _tilemap.WorldToCell(position);
+			Gizmos.color = _tilemap.HasTile(cellPosition0) && !_tilemap.HasTile(cellPosition0 + Vector3Int.up) ? Color.red : Color.green;
+			Gizmos.DrawWireCube(_tilemap.GetCellCenterWorld(cellPosition0), Vector3.one);
+		}
 		
 		// 穴のギズモ表示
 		position = new Vector2(x, boxCollider2D.bounds.min.y - boxCollider2D.size.y);
-		cellPosition = _tilemap.WorldToCell(position);
+		var cellPosition = _tilemap.WorldToCell(position);
 		var size = Vector3Int.CeilToInt(boxCollider2D.size);
 		var bounds = new BoundsInt(cellPosition.x, cellPosition.y, 1, size.x, size.y, 1);
 		var isHole = _tilemap.GetTilesBlock(bounds) == null;
 		Gizmos.color = isHole ? Color.red : Color.green;
 		Gizmos.DrawWireCube(bounds.center, bounds.size);
 		
-		// x = _moveDirection.x >= 0 ? boxCollider2D.bounds.max.x + 1 : boxCollider2D.bounds.min.x - 1;
-		
 		// 壁のギズモ表示
-		position = new Vector2(x, boxCollider2D.bounds.min.y + wallHeight - 1);
-		cellPosition = _tilemap.WorldToCell(position);
-		Gizmos.color = _tilemap.HasTile(cellPosition) ? Color.red : Color.green;
-		Gizmos.DrawWireCube(_tilemap.GetCellCenterWorld(cellPosition), Vector3.one);
+		var height = boxCollider2D.bounds.size.y - (autoJump ? autoJumpHeight : 0);
+		for (y = 0; y < height; y++)
+		{
+			position = new Vector2(x, boxCollider2D.bounds.min.y + (autoJump ? autoJumpHeight : 0) + y);
+			cellPosition = _tilemap.WorldToCell(position);
+			Gizmos.color = _tilemap.HasTile(cellPosition) ? Color.magenta : Color.blue;
+			Gizmos.DrawWireCube(_tilemap.GetCellCenterWorld(cellPosition), Vector3.one);
+		}
 	}
 }
