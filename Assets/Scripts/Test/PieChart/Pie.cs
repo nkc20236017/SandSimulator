daa8cc3272378b279f607b663cad715607a5d5f2
nkc20230631 <nkc23.watanabe.mio@gmail.com>
2024-09-29 06:27:31 +0900

    ［追加］仮の円画像の追加、ブロックの種類にNoneを追加

---
+++ Assets/Scripts/Test/PieChart/Pie.cs
@@ -0,0 +1,53 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class Pie : MonoBehaviour
+{
+	private BlockType _blockType;
+	private int _value;
+	private Image _image;
+	
+	public BlockType BlockType => _blockType;
+	public int Value => _value;
+
+	private void Awake()
+	{
+		_image = GetComponent<Image>();
+	}
+
+	private void Start()
+	{
+		ClearPie();
+	}
+
+	private void Update()
+	{
+		_image.fillAmount = _value / 180f;
+	}
+
+	public void ClearPie()
+	{
+		gameObject.SetActive(false);
+		_blockType = BlockType.None;
+		_image.fillAmount = 0;
+		_image.sprite = null;
+		_value = 0;
+	}
+
+	public void SetBlockType(BlockType blockType, Sprite sprite)
+	{
+		gameObject.SetActive(true);
+		_blockType = blockType;
+		_image.sprite = sprite;
+	}
+	
+	public void SetValue(int value)
+	{
+		_value = value;
+	}
+	
+	public void SetRotation(int value)
+	{
+		transform.localRotation = Quaternion.Euler(0, 0, -value);
+	}
+}
---
+++ Assets/Scripts/Test/PieChart/PieChart.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Linq;
+using UnityEngine;
+
+public class PieChart : MonoBehaviour
+{
+	[Header("Data Config")]
+	[SerializeField] private BlockDatas blockDatas;
+	[SerializeField] private Pie piePrefab;
+	
+	[Header("Pie Chart Config")]
+	[SerializeField] private PieChartData[] pieChartDatas;
+	[SerializeField] private int maxPieCount;
+	[SerializeField] private int maxTankValue;
+	
+	private Pie[] _pies;
+
+	private void Start()
+	{
+		_pies = new Pie[maxPieCount];
+		for (var i = 0; i < maxPieCount; i++)
+		{
+			_pies[i] = Instantiate(piePrefab, transform);
+		}
+	}
+
+	private void Update()
+	{
+		if (pieChartDatas.Length == 0) { return; }
+		if (_pies.Sum(pie => pie.Value) >= maxTankValue) { return; }
+		
+		for (var i = 0; i < pieChartDatas.Length; i++)
+		{
+			if (i >= maxPieCount) { break; }
+			
+			if (pieChartDatas[i].value == 0)
+			{
+				_pies[i].ClearPie();
+				continue;
+			}
+			
+			var blocktype = pieChartDatas[i].blockType;
+			Sprite sprite;
+			sprite = blocktype == BlockType.Ore ? blockDatas.GetOre(pieChartDatas[i].oreType).sprite : blockDatas.GetBlock(blocktype).sprite;
+			_pies[i].SetBlockType(blocktype, sprite);
+			_pies[i].SetValue(pieChartDatas[i].value);
+			var value = pieChartDatas.Take(i).Sum(pie => pie.value);
+			_pies[i].SetRotation(value * 180 / maxTankValue);
+		}
+	}
+}
+
+[Serializable]
+public class PieChartData
+{
+	public BlockType blockType;
+	public OreType oreType;
+	public MagicOreType magicOreType;
+	public int value;
+}
