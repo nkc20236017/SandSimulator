2f2a12e7333d6db05e9b7a967d5e428f67e79bba
SAIKOUKIROKU\xjiji <xjijian29@gmail.com>
2024-10-03 00:54:52 +0900

    [追加]ResultのTextを追加

--- Assets/Scripts/Result/Presenter/ResultAnimationPresenter.cs
+++ Assets/Scripts/Result/Presenter/ResultAnimationPresenter.cs
@@ -1,8 +1,8 @@
 using Cysharp.Threading.Tasks;
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using System.Threading;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ResultAnimationPresenter : MonoBehaviour
 {
@@ -10,19 +10,25 @@ public class ResultAnimationPresenter : MonoBehaviour
     private GameObject paperAnimationObject;
     [SerializeField]
     private Transform paperTransform;
+    [SerializeField]
+    private Text resultText;
 
-    public async UniTask AnimationPaper(ResultOutPutData outPutData , CancellationToken cancellationToken)
+    public async UniTask AnimationPaper(ResultOutPutData outPutData, CancellationToken cancellationToken)
     {
+        int resultPrie = 0;
         for (int i = 0; i < outPutData.mineralTank.Count; i++)
         {
-            var paperObject = Instantiate(paperAnimationObject,paperTransform);
+            var paperObject = Instantiate(paperAnimationObject, paperTransform);
             var animationPaper = paperObject.GetComponent<ResultAnimationUI>();
             var mineral = outPutData.mineralTank[i];
             var mineralPrice = mineral.mineralData.price * mineral.mineralAmount;
+            resultPrie += mineralPrice;
             animationPaper.SetUpUI(mineral.mineralData.resultSprite, mineral.mineralAmount.ToString(), mineralPrice.ToString());
             await animationPaper.AnimationEnterUI(cancellationToken);
             await animationPaper.AnimationStampUI(cancellationToken);
             await animationPaper.AnimationExitUI(cancellationToken);
         }
+
+        resultText.text = resultPrie.ToString();
     }
 }
