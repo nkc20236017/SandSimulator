0ad3e410b6e510479015750c193001b5a80f598b
saikoukiroku810 <xjijian29@gmail.com>
2024-10-02 10:15:09 +0900

    [追加]Playerが死んだときにタイトルに戻します

--- Assets/Scripts/SceneManager/SceneEntoryPoint/MainGameEntoryPoint.cs
+++ Assets/Scripts/SceneManager/SceneEntoryPoint/MainGameEntoryPoint.cs
@@ -14,15 +14,15 @@ public class MainGameEntoryPoint : SceneEntryPointBase
     protected override async UniTask OnInitialize(ISceneDataReader reader, IProgress<IProgressDataStore> progress, CancellationToken cancellationToken)
     {
         var store = new ProgressDataStore<ProgressData>();
-        await UniTask.Delay(TimeSpan.FromSeconds(0.2f));
+        await UniTask.Delay(TimeSpan.FromSeconds(0.2f),cancellationToken:cancellationToken);
         progress.Report(store.SetData(progressData));
-        await UniTask.Delay(TimeSpan.FromSeconds(3f));
+        await UniTask.Delay(TimeSpan.FromSeconds(3f), cancellationToken: cancellationToken);
         progress.Report(store.SetData(progressData));
-        await UniTask.Delay(TimeSpan.FromSeconds(3f));
+        await UniTask.Delay(TimeSpan.FromSeconds(3f), cancellationToken: cancellationToken);
         progress.Report(store.SetData(progressData));
-        await UniTask.Delay(TimeSpan.FromSeconds(3f));
+        await UniTask.Delay(TimeSpan.FromSeconds(3f), cancellationToken: cancellationToken);
         progress.Report(store.SetData(progressData));
-        await UniTask.Delay(TimeSpan.FromSeconds(2f));
+        await UniTask.Delay(TimeSpan.FromSeconds(2f), cancellationToken: cancellationToken);
         progress.Report(store.SetData(progressData));
     }
 
