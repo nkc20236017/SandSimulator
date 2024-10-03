f8f742e5390a3b79e5996e59d40065c0900476ab
SAIKOUKIROKU\xjiji <xjijian29@gmail.com>
2024-10-03 23:30:36 +0900

    [修正]Resultアニメーションの修正をしました。

--- Assets/Scripts/Result/Presenter/TotalResultPresenter.cs
+++ Assets/Scripts/Result/Presenter/TotalResultPresenter.cs
@@ -6,33 +6,18 @@ public class TotalResultPresenter : MonoBehaviour
 {
     [SerializeField]
     private GameObject uiPrefab;
-    [SerializeField]
-    private Text resultText;
+
 
     public void TotalUI(ResultOutPutData outPutData)
     {
-        int resultPrice = 0;
+
         for (int i = 0; i < outPutData.mineralTank.Count; i++)
         {
-
             var resultMoney = outPutData.mineralTank[i].mineralAmount * outPutData.mineralTank[i].mineralData.price;
             var uiObject = Instantiate(uiPrefab, transform);
             uiObject.GetComponent<ResultUI>().SetUpUI(outPutData.mineralTank[i].mineralData.resultSprite
                 , outPutData.mineralTank[i].mineralAmount.ToString(), resultMoney.ToString());
-            resultPrice += resultMoney;
         }
-
-        DOVirtual.Int
-            (
-            from: 0,
-            to: resultPrice,
-            duration : 3,
-            onVirtualUpdate: (totalePrice) =>
-            {
-
-                resultText.text = totalePrice.ToString();
-            }
-            );
 
     }
 
