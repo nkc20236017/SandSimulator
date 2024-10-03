d023f9e77b7fa3e8525114002d270284c71e8e3e
SAIKOUKIROKU\xjiji <xjijian29@gmail.com>
2024-10-03 22:26:31 +0900

    [追加]リザルトのTextアニメーションを作成

--- Assets/Scripts/Result/Presenter/TotalResultPresenter.cs
+++ Assets/Scripts/Result/Presenter/TotalResultPresenter.cs
@@ -1,5 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -16,12 +15,26 @@ public class TotalResultPresenter : MonoBehaviour
         for (int i = 0; i < outPutData.mineralTank.Count; i++)
         {
 
-            var resultMoney = outPutData.mineralTank[i].mineralAmount*outPutData.mineralTank[i].mineralData.price;
-            var uiObject = Instantiate(uiPrefab,transform);
+            var resultMoney = outPutData.mineralTank[i].mineralAmount * outPutData.mineralTank[i].mineralData.price;
+            var uiObject = Instantiate(uiPrefab, transform);
             uiObject.GetComponent<ResultUI>().SetUpUI(outPutData.mineralTank[i].mineralData.resultSprite
-                , outPutData.mineralTank[i].mineralAmount.ToString(),resultMoney.ToString());
+                , outPutData.mineralTank[i].mineralAmount.ToString(), resultMoney.ToString());
             resultPrice += resultMoney;
         }
-        resultText.text = resultPrice.ToString();
+
+        DOVirtual.Int
+            (
+            from: 0,
+            to: resultPrice,
+            duration : 3,
+            onVirtualUpdate: (totalePrice) =>
+            {
+
+                resultText.text = totalePrice.ToString();
+            }
+            );
+
     }
+
+
 }
