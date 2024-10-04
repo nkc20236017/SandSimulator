a6b8e79afaba074909b43d0b0168029b03f79026
saikoukiroku810 <xjijian29@gmail.com>
2024-10-04 13:27:03 +0900

    [修正]一時的に消したscriptを復元

--- Assets/Scripts/SceneManager/Demo/DemoInputTitleScene.cs
+++ Assets/Scripts/SceneManager/Demo/DemoInputTitleScene.cs
@@ -10,6 +10,14 @@ public class DemoInputTitleScene : MonoBehaviour
     private ISceneIdentifier gameScene = new BuiltInSceneIdentifier("SelectScene");
     private bool fastSelect;
 
+    private void Start()
+    {
+        if (!SceneManager.GetSceneByName("RootScene").isLoaded)
+        {
+            SceneManager.LoadScene("RootScene", LoadSceneMode.Additive);
+        }
+    }
+
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
