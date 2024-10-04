5dbbd76c7a6080b0b6df3078c585097c6629cf19
nkc20230631 <nkc23.watanabe.mio@gmail.com>
2024-10-04 15:09:43 +0900

    ［追加］カーソルの設定

---
+++ Assets/Scripts/Game/Cursor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NaughtyAttributes;
+using UnityEngine;
+
+public enum CursorType
+{
+	None,
+	Game,
+	UI,
+	OnButton,
+}
+
+[CreateAssetMenu(fileName = "Cursor", menuName = "ScriptableObjects/Cursor")]
+public class Cursor : ScriptableObject
+{
+	public CursorData[] cursorData;
+}
+
+[Serializable]
+public class CursorData
+{
+	public CursorType cursorType;
+	[ShowAssetPreview] public Texture2D cursorTexture;
+	public Vector2 hotSpot;
+}
