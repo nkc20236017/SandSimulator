86a4dc4140e159e31594ed35dc0736f8ef76985e
nkc20236031 <nkc23.watanabe.mio@gmail.com>
2024-10-05 16:54:33 +0900

    ［追加］モグラFSM

--- Assets/Scripts/Character/Enemy/FiniteStateMachine/Actions/Mole/ActionWanderMole.cs
+++ Assets/Scripts/Character/Enemy/FiniteStateMachine/Actions/Mole/ActionWanderMole.cs
@@ -1,12 +1,87 @@
 ﻿using UnityEngine;
+using NaughtyAttributes;
+using Random = UnityEngine.Random;
 
 public class ActionWanderMole : FsmAction
 {
+	[Header("Wander Config")]
+	[SerializeField, MinMaxSlider(0f, 60f)] private Vector2 _wanderTime;
+	[SerializeField] private float _radius;
 	
+	private float _timer;
+	private float _randomWanderTime;
+	private Vector3 _movePosition;
+	private EnemyBrain _enemyBrain;
+	private IChunkInformation _chunkInformation;
+	
+	private void Start()
+	{
+		_timer = Random.Range(_wanderTime.x, _wanderTime.y);
+		GetRandomPointInCircle();
+	}
 	
 	public override void Action()
 	{
-		
+		Wander();
+	}
+	
+	private void Wander()
+	{
+		if (IsTilemap(_movePosition))
+		{
+			GetRandomPointInCircle();
+			return;
+		}
+
+		Movement();
+
+		_timer -= Time.deltaTime;
+		if (_timer <= 0f)
+		{
+			GetRandomPointInCircle();
+			_timer = Random.Range(_wanderTime.x, _wanderTime.y);
+		}
+	}
+        
+    private void Movement()
+    {
+        // ランダムな位置に向かって移動する
+        var moveDirection = (_movePosition - transform.position).normalized;
+        var movement = moveDirection * (_enemyBrain.Status.speed * Time.deltaTime);
+        if (Vector3.Distance(transform.position, _movePosition) >= 0.5f)
+        {
+            transform.Translate(movement);
+        }
+    }
+
+    private void GetRandomPointInCircle()
+    {
+        // 範囲内のランダムな位置を取得する
+        var randomAngle = Random.Range(0f, Mathf.PI * 2f);
+        var randomDistance = Random.Range(0f, _radius);
+        var x = Mathf.Cos(randomAngle) * randomDistance;
+        var y = Mathf.Sin(randomAngle) * randomDistance;
+        _movePosition = transform.position + new Vector3(x, y, 0f);
+    }
+    
+    private bool IsTilemap(Vector3 position)
+    {
+        // マップがあるかどうか調べる
+		var mapTilemap = _chunkInformation.GetChunkTilemap(position);
+		return mapTilemap != null;
+    }
+
+    private void OnDrawGizmos()
+    {
+	    Gizmos.color = Color.green;
+		Gizmos.DrawWireSphere(transform.position, _radius);
+    }
+
+    private void OnEnable()
+	{
+		var worldMapManager = FindObjectOfType<WorldMapManager>();
+		_chunkInformation = worldMapManager.GetComponent<IChunkInformation>();
+		_enemyBrain = GetComponent<EnemyBrain>();
 	}
 }
 
