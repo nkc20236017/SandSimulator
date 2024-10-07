a76867dc9042f17c3e30825b063344d95399c761
nkc20236005 <nkc23.satou_hajim@gnail.com>
2024-10-07 16:52:38 +0900

    [追加]エネミーの点滅

--- Assets/Scripts/Character/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Character/Enemy/EnemyHealth.cs
@@ -1,19 +1,47 @@
 ﻿using UnityEngine;
+using DG.Tweening;
+using System.Collections;
 
 public class EnemyHealth : MonoBehaviour, IDamageable
 {
     public float CurrentHealth { get; set; }
+    [SerializeField]
+    private SpriteRenderer sprite;
+    [SerializeField]
+    private int count;
+    [SerializeField]
+    private float time;
+    private int x;
 
     public void TakeDamage(int damage)
     {
         CurrentHealth -= damage;
         // TODO: ［効果音］エネミーダメージ
         AudioManager.Instance.PlaySFX("DamegeSE");
-
+        StartCoroutine(ColorChenge());
         if (CurrentHealth > 0f) { return; }
-
         DisableEnemy();
     }
+
+    private IEnumerator ColorChenge()
+    {
+        for (int z = 0; z > count; z++)
+        {
+            if(x != 0)
+            {
+                yield return new WaitForSeconds(time);
+                sprite.DOColor(new Color(255, 255, 255, x), time);
+                x = 0;
+            }
+            else
+            {
+                yield return new WaitForSeconds(time);
+                sprite.DOColor(new Color(255, 255, 255, x), time);
+                x = 255;
+            }
+        }
+    }
+        
 
     private void DisableEnemy()
     {
@@ -27,4 +55,3 @@ public class EnemyHealth : MonoBehaviour, IDamageable
         Destroy(gameObject);
     }
 }
-
