a121fee841512938ca4a90254f3e7335c74d0935
nkc20230631 <nkc23.watanabe.mio@gmail.com>
2024-10-11 14:52:12 +0900

    ［追加］言われたとおりに実装した層のスライド表示

---
+++ Assets/Scripts/Map/Layer/Layer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Layer", menuName = "ScriptableObjects/Layer")]
+public class Layer : ScriptableObject
+{
+	public LayerData[] layerData;
+}
+
+[Serializable]
+public class LayerData
+{
+	public string layerName;
+	public int degreeOfRisk;
+}
